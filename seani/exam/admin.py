2666e3f71905762525bd4f46cf5c4874fdf43155
SantiagoAHS <lionelsanti2018@gmail.com>
2024-02-29 11:04:34 -0600

    05 Update models exam

--- seani/exam/admin.py
+++ seani/exam/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
+from .models import Stage, Exam, ExamModule
+
+# Register your models here.
 
 from .models import Stage
-# Register your models here.
+
 @admin.register(Stage)
 class StageAdmin(admin.ModelAdmin):
-    list_display = ['stage','month','year']
-    
+    list_display = ['stage', 'month', 'year']
 
+class ExamModuleInline(admin.TabularInline):
+    model = ExamModule
+    extra = 1
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ['user', 'score', 'career']
+    inlines =[ExamModuleInline]
--- seani/exam/models.py
+++ seani/exam/models.py
@@ -1,28 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
+from career.admin import Carrer
+from library.models import Module
+
 
 # Create your models here.
-
-
 class Stage(models.Model):
 
-    stage = models.IntegerField(
-        verbose_name = "Etapa")
-    aplication_date = models.DateField(
-        verbose_name="Fechha de Aplicacion")
 
+
+    stage = models.IntegerField(
+        verbose_name =  "Etapa",
+        max_length=2
+        )
+    application_date = models.DateField(
+        verbose_name = "Fecha de aplicacion"
+    )
+    
     @property
     def year(self):
-        return self.aplication_date.year
+        return self.application_date.year
     
     @property
     def month(self):
-        months = ['enero','febrero','marzo','abril','mayo','julio','junio','agosto','septiembre','octubre','noviembre','diciembre']
-        return months[self.aplication_date.month - 1]
-    
+        months = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio' , 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
+
+        return months[self.application_date.month - 1]
     
     def __str__(self):
-        return f"{ self.stage}-{ self.month }-{self.year}"
-
+        return f"{ self.stage } - {self.month } - { self.year }"
+    
     class Meta:
-        verbose_name="etapa"
-        verbose_name_plural="etapas"
+        verbose_name = "Etapa"
+        verbose_name_plural = "Etapas"
+
+class Exam(models.Model):
+    user = models.OneToOneField(User,
+                                on_delete = models.CASCADE,
+                                verbose_name = "Usuario")
+    stage = models.ForeignKey(Stage,
+                              on_delete = models.CASCADE,
+                              verbose_name = "Etapa")
+    career = models.ForeignKey(Carrer,
+                              on_delete = models.CASCADE,
+                              verbose_name = "Carrera")
+    modules = models.ManyToManyField(Module,
+                                     through='ExamModule',
+                                     verbose_name = "Modulos")
+    score = models.FloatField(verbose_name = "Calificación",
+                              default = 0.0)
+
+
+class ExamModule(models.Model):
+    exam = models.ForeignKey(Exam,
+                              on_delete = models.CASCADE,
+                              verbose_name = "Examen")
+    module = models.ForeignKey(Module,
+                              on_delete = models.CASCADE,
+                              verbose_name = "Modulo")
+    active = models.BooleanField(default = True,
+                                 verbose_name = "Activo")
+    score = models.FloatField(default = 0.0,
+                              verbose_name = "Calificacion")
